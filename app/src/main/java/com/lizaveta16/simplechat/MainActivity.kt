ebf058b0676c28e0d67f44b62149c11cc7ea9bb1
Lizaveta <lizazaytseva16@gmail.com>
2021-11-30 21:33:03 +0300

    Added ui

--- app/src/main/java/com/lizaveta16/simplechat/MainActivity.kt
+++ app/src/main/java/com/lizaveta16/simplechat/MainActivity.kt
@@ -2,16 +2,38 @@ package com.lizaveta16.simplechat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.lizaveta16.simplechat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
         val myRef: DatabaseReference = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.sendBut.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+
+        onChangeListener(myRef)
+    }
+
+    private fun onChangeListener(dRef : DatabaseReference){
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append(snapshot.value.toString())
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
